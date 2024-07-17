e3da8c9a11fecead0a8386d46f7968fa58283829
Nay Lin Aung <naylinaung.nla.dev@gmail.com>
2024-07-17 17:39:20 +0700

    data test

--- components/MiniApp.vue
+++ components/MiniApp.vue
@@ -1,9 +1,36 @@
 <script lang="ts" setup>
 import { MainButton, useWebAppPopup } from 'vue-tg'
+import { useWebApp } from 'vue-tg'
+
+const initData = useWebApp().initData
+const initDataUnsafe = useWebApp().initDataUnsafe
 
 const { showAlert } = useWebAppPopup()
 </script>
 
 <template>
-  <MainButton text="Open alert" @click="() => showAlert('Hello!')" />
+    <h1>Mini App - Shop Test</h1>
+
+    <label for="initData">initData</label>
+    <textarea class="w-100" name="initData">
+        {{ initData }}
+    </textarea>
+
+    <label for="initDataUnsafe">initDataUnsafe</label>
+    <textarea class="w-100" name="initDataUnsafe">
+        {{ initDataUnsafe }}
+    </textarea>
+    <MainButton text="Open alert" @click="() => showAlert('Hello!')" />
 </template>
+
+<style scoped>
+h1{
+    text-align: center;
+}
+textarea{
+    margin: 0;
+    width: 100%;
+    height: 200px;
+    padding: 0;
+}
+</style>
